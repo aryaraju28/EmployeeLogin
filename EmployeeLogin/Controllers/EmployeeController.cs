1acac489494d3451d0902d6e4c04eab4ca006927
aryaraju28 <arya28199@gmail.com>
2021-05-02 12:21:35 +0530

    Add project files.

---
+++ EmployeeLogin/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using EmployeeLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeLogin.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        String connectionString = "Server=RAED_COMPUTER\\SQLEXPRESS;Database=EmployeeManagement;Trusted_Connection=True;";
+        [HttpGet]
+        public Employee EmployeLogin(string username, string password)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlCommand command = new SqlCommand("EmployeLogin", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("Username", username);
+            command.Parameters.AddWithValue("Password", password);
+
+
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            Employee employee = new Employee();
+            employee.Id = Convert.ToInt32(reader["Id"]);
+            employee.Name = reader["Name"].ToString();
+            employee.Age = Convert.ToInt32(reader["Age"]);
+            employee.Salary = Convert.ToInt32(reader["Salary"]);
+           
+
+
+            reader.Close();
+            con.Close();
+            return employee;
+
+
+
+        }
+    }
+}
---
+++ EmployeeLogin/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeLogin.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary  { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
